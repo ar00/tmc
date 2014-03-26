2fafbd8c86d0ddb8e20b999658ab349b373b92d1
ar00 <re@science.unitn.it>
2014-03-26 14:39:44 +0100

    updated annotation

--- annotation.R
+++ annotation.R
@@ -1,18 +1,33 @@
 
 ### date: 19/03/2014
 ### aim: 
-### - retrieve IPI-HGNC relations directly from NCI60 RNA processed data
-### - retrieve genomic ccordinates
+### - retrieve IPI-HGNC relations directly from NCI60 files with RNA and protein processed data
+### - retrieve genomic ccordinates for genes profiled at either level in the NCI60 panel
 ### output: 
-### - ./annotation/ipi.hgnc.nci60.txt
+### - ./annotation/nci60annotation.txt
 ### - annotation.rda
 
 
-# load Ensembl BioMart genomic annotations and NCI60 processed data
+# load Ensembl BioMart genomic annotations and NCI60 processed data at either RNA or protein level
 load("data.rda")
 
+
+# retrieve IPI-HGNC relations for genes profiled at the protein level 
+symbol.start = as.numeric(sapply(prot$Fasta.headers, function(x)(regexpr("Gene_Symbol",x)[1])))
+ss = substr(prot$Fasta.headers[symbol.start != -1], start=symbol.start[symbol.start != -1], stop=1000000L)
+symbol = gsub("Gene_Symbol=","",as.vector(sapply(ss, function(x)(strsplit(x," ")[[1]][1]))))
+zrs = rep(0,length(symbol))
+ipiHgncProt = data.frame(IPI=zrs,HGNC.symbol=zrs)
+ipiHgncProt[,1] = prot$Accession[symbol.start != -1]
+ipiHgncProt[,2] = symbol
+# retrieve IPI-HGNC relations for genes profiled at the mRNA level
+ipiHgncRna = unique(rna[,2:3])
+colnames(ipiHgncRna) = c("IPI","HGNC.symbol")
+# retrieve IPI-HGNC relations for genes profiled at either level
+ipiHgnc = unique(rbind(ipiHgncProt,ipiHgncRna))
+
+
 # retrieve genomic annotation for NCI60 RNA profiled genes
-ipiHgnc = unique(rna[,2:3]) 
 l = dim(ipiHgnc)[1]
 annotation = data.frame(ID = character(1), IPI = character(l), HGNC.symbol = character(l), Ensembl.Gene.ID = character(l), 
  Chromosome.Name =  character(l), Gene.Start..bp. = integer(l), Gene.End..bp. = integer(l), Strand = character(l))
